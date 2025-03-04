ebc38495ffc978b64c260ea7231d7af59a31830a
Capuozzolo <giusepperadatti33@gmail.com>
2025-03-04 09:50:38 +0100

    55

--- Drive.js
+++ Drive.js
@@ -247,7 +247,6 @@ const mediaFiles = [
     { type: 'video', src: 'img/altro/5.mp4', name: 'video altro 5.mp4', description: '' },
 
 
-
     /*{ type: 'divider' }, 
 
 
@@ -287,18 +286,18 @@ mediaFiles.forEach(file => {
         hr.style.height = '15px';
         hr.style.backgroundColor = '#000000bb';
         hr.style.width = '100%';
-        hr.style.margin = '30px 0';
-        hr.style.gridColumn = '1 / -1';
+        hr.style.margin = '30px 0'; // Spazio sopra e sotto il divisore
+        hr.style.gridColumn = '1 / -1'; // Fa sì che l'elemento prenda tutta la larghezza
         mediaGrid.appendChild(hr);
-        return;
+        return; // Salta il resto del codice poiché non serve aggiungere altro per il divisore
     }
 
     if (file.type === 'sectionTitle') {
         const sectionTitle = document.createElement('div');
         sectionTitle.textContent = file.text;
-        sectionTitle.className = 'section-title';
+        sectionTitle.className = 'section-title'; // Usa la classe CSS definita
         mediaGrid.appendChild(sectionTitle);
-        return;
+        return; // Salta il resto del codice poiché non serve aggiungere altro per il titolo
     }
 
     const gridItem = document.createElement('div');
@@ -373,9 +372,12 @@ window.onclick = function(event) {
 document.addEventListener("DOMContentLoaded", function() {
     const gridItems = document.querySelectorAll('.grid-item');
 
-    gridItems.forEach((item, index) => {
-        setTimeout(() => {
-            item.classList.add('show'); // Aggiungi la classe che attiva l'animazione
-        }, 25 * index); // Ritardo per animare le immagini in sequenza
+    gridItems.forEach(item => {
+        const mediaElement = item.querySelector('img, video');
+        if (mediaElement) {
+            mediaElement.addEventListener('load', () => {
+                // Eventuale logica da aggiungere se necessario quando l'immagine/video è caricato
+            });
+        }
     });
 });
